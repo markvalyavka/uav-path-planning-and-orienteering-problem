db1508684749bc0abb797962df2f5f4d4e32c560
Mark Valyavka <v221101mark@gmail.com>
2023-01-22 23:42:45 +0000

    add KOP dataset file parser script

---
+++ python/problem_txt_to_yaml.py
@@ -0,0 +1,39 @@
+import yaml
+
+txt_filename = "p1.4.q.txt"
+yaml_filename = "../new_config.yaml"
+
+start_location = end_location = None
+locations = []
+rewards = []
+
+with open(txt_filename, "r") as f:
+    f.readline()
+    f.readline()
+    tmax = float(f.readline().strip().split(" ")[1])
+    lines = f.readlines()
+    for l in lines:
+        loc = list(map(float, l.strip().split("\t")))
+        rewards.append(loc[2])
+        loc[2] = 0
+        locations.append(loc)
+    start_location = locations[0]
+    end_location = locations[-1]
+    locations.pop(0)
+    locations.pop()
+    rewards.pop(0)
+    rewards.pop()
+
+with open(yaml_filename, "r") as fa:
+    yaml_cfg = yaml.full_load(fa)
+    yaml_cfg['t_max'] = tmax
+    yaml_cfg['start']['position'] = start_location
+    yaml_cfg['end']['position'] = end_location
+    yaml_cfg['locations'] = locations
+
+with open(yaml_filename, "w") as fa:
+    yaml.dump(yaml_cfg, fa, default_flow_style=None)
+
+
+
+
